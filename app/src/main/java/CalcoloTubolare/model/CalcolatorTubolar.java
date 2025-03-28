06420131ab50321d248fa23f2d25bdbfe0e5970d
Mattia Cavina <mattia.cavina2@studio.unibo.it>
2025-03-28 16:54:36 +0100

    AutoUpDate

--- app/src/main/java/CalcoloTubolare/model/CalcolatorTubolar.java
+++ app/src/main/java/CalcoloTubolare/model/CalcolatorTubolar.java
@@ -12,6 +12,8 @@ import CalcoloTubolare.model.api.Tubolar;
 
 public class CalcolatorTubolar {
 
+    private static final int LAMA = 2;
+
     public static HashMap<String, LinkedList<Pair<Integer, LinkedList<Integer>>>> calcoloTotal(
             HashMap<String, Set<Tubolar>> multiQueue) {
         HashMap<String, LinkedList<Pair<Integer, LinkedList<Integer>>>> mapCut = new HashMap<>();
@@ -37,8 +39,14 @@ public class CalcolatorTubolar {
             for (Entry<String, LinkedList<Pair<Integer, LinkedList<Integer>>>> elemEntry : mapCut.entrySet()) {
                 out = out + elemEntry.getKey() + "\n";
                 for (Pair<Integer, LinkedList<Integer>> elem : elemEntry.getValue()) {
-                    out = out + "Lunghezza tubolare:" + elem.getValue0() + "\n";
-                    out = out + elem.getValue1().toString() + "\n";
+                    out = out + "Lunghezza:" + elem.getValue0() + "\n";
+                    // out = out + elem.getValue1().toString() + "\n";
+                    out = out
+                            + elem.getValue1().stream()
+                                    .map(t -> "L" + t + " Numero:"
+                                            + elem.getValue1().stream().mapToInt(e -> e).sum() / t)
+                                    .distinct().toList()
+                            + "\n";
                 }
                 out = out + "Numero Tubolari:" + elemEntry.getValue().size() + "\n" + "\n";
             }
@@ -58,7 +66,7 @@ public class CalcolatorTubolar {
                 out = out + elemEntry.getKey() + "\n";
                 out = out + "Lunghezza tubolare:" + elemEntry.getValue().getFirst().getValue0() + "\n";
 
-                out = out + "Numero Tubolari:" + elemEntry.getValue().size() + "\n" + "\n" ;
+                out = out + "Numero Tubolari:" + elemEntry.getValue().size() + "\n" + "\n";
             }
         } else {
             throw new IllegalArgumentException();
@@ -71,25 +79,25 @@ public class CalcolatorTubolar {
             int lenght) {
         LinkedList<Pair<Integer, LinkedList<Integer>>> listList = new LinkedList<>();
 
-        Set<Tubolar> temp = new TreeSet<>((o1, o2) -> (int) o2.getLenght() - (int) o1.getLenght());
+        Set<Tubolar> temp = new TreeSet<>((o1, o2) -> o2.getLenght() - o1.getLenght());
 
         for (Tubolar elemTubolar : tempList) {
             temp.add(new Tubolar(elemTubolar.getLenght(), elemTubolar.getQuantity()));
         }
 
-        while (temp.stream().mapToInt(t -> t.getQuantity()).sum() != 0) {
+        while (temp.stream().mapToInt(Tubolar::getQuantity).sum() != 0) {
 
-            int total1 = lenght;
+            int total = lenght;
 
             LinkedList<Integer> list = new LinkedList<>();
             Pair<Integer, LinkedList<Integer>> pairList = new Pair<Integer, LinkedList<Integer>>(
                     Integer.valueOf(lenght), list);
             for (Tubolar elem : temp) {
 
-                while (elem.getQuantity() > 0 && (total1 - (int) elem.getLenght()) >= 0) {
+                while (elem.getQuantity() > 0 && (total - elem.getLenght()) >= 0) {
                     list.add(elem.getLenght());
                     elem.setQuantity(elem.getQuantity() - 1);
-                    total1 = total1 - (int) elem.getLenght();
+                    total = total - elem.getLenght() - LAMA;
                 }
             }
             listList.add(pairList);
